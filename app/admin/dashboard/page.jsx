605c8cd7e1028995ea215d9685ccf5a8f4c4b3bb
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-07-07 17:43:10 +0530

    first commit

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -1,5 +1,9 @@
-const Dashboard = () => {
+"use client"
+import {useParams} from "next/navigation";
 
+const Dashboard = () => {
+    const params = useParams()
+    alert(params)
     return <div className="p-4 bg-white/20 backdrop-blur-sm rounded-xl border-none h-full">Dashboard</div>
 }
 
