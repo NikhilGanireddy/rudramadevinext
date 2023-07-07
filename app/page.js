bf0d0356a2931b19dae3de4d1b44f76ab0232130
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-07-07 18:33:14 +0530

    sample

--- app/page.js
+++ app/page.js
@@ -1,32 +1,37 @@
-"use client"
+"use client";
 import Link from "next/link";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import axios from "axios";
-import {useGlobalUserContext} from "@/context/userContext";
+import { useGlobalUserContext } from "@/context/userContext";
 
 export default function Home() {
-    const {userData, setUserData, ready, setReady} = useGlobalUserContext()
+  const { userData, setUserData, ready, setReady } = useGlobalUserContext();
 
+  return (
+    <div className="p-4 rounded-xl border h-full flex flex-col justify-center items-center gap-6">
+      <h1>Rudramadevi Girls Hostel</h1>
 
-    if (ready && !userData) {
-        return <Link href={"/login"}/>
-    }
+      <div>
+        {ready && !userData && (
+          <Link
+            className="bg-black text-gray-300 px-6 py-2 rounded-lg border"
+            href={"/login"}
+          >
+            Login
+          </Link>
+        )}
 
-    return (
-        <div className="p-4 rounded-xl border h-full flex flex-col justify-center items-center gap-6">
-            <h1>Rudramadevi Girls Hostel</h1>
-
-            <div>
-                {(ready && !userData) &&
-                    <Link className="bg-black text-gray-300 px-6 py-2 rounded-lg border" href={"/login"}>Login</Link>}
-
-                {(ready && userData) && <Link className="bg-black text-gray-300 px-6 py-2 rounded-lg border"
-                                              href={`/${(userData?.isAdmin) ? "admin" : "user"}/dashboard`}>Go to
-                    Dashboard</Link>
-                }
-            </div>
-        </div>
-    )
+        {ready && userData && (
+          <Link
+            className="bg-black text-gray-300 px-6 py-2 rounded-lg border"
+            href={`/${userData?.isAdmin ? "admin" : "user"}/dashboard`}
+          >
+            Go to Dashboard
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 }
 
 // <Link className="bg-black text-gray-300 px-6 py-2 rounded-lg border"
