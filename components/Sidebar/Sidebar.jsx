ec2a11a4faed6ab334ab8ac3b04370a858baa5e2
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-07-07 17:50:01 +0530

    another one key prop

--- components/Sidebar/Sidebar.jsx
+++ components/Sidebar/Sidebar.jsx
@@ -2,14 +2,27 @@ import sidebar from "@/data/sidebar";
 import Link from "next/link";
 
 const Sidebar = () => {
-    return <div className=" col-span-2 h-full p-2 bg-white/10 backdrop-blur-sm rounded-xl border-none">
-        <h1 className="px-4 py-2"><Link href={"/"}>Rudramadevi</Link></h1>
-        <div className=" mt-12 flex flex-col gap-2">
-            {sidebar.map((navlnk) => {
-                return <Link id={navlnk.id} href={navlnk.url} className="px-4 py-2 rounded-lg transition-all duration-200 cursor-pointer hover:bg-gray-300 hover:text-black">{navlnk.title}</Link>
-            })}
-        </div>
+  return (
+    <div className=" col-span-2 h-full p-2 bg-white/10 backdrop-blur-sm rounded-xl border-none">
+      <h1 className="px-4 py-2">
+        <Link href={"/"}>Rudramadevi</Link>
+      </h1>
+      <div className=" mt-12 flex flex-col gap-2">
+        {sidebar.map((navlnk) => {
+          return (
+            <Link
+              id={navlnk.id}
+              href={navlnk.url}
+              key={navlnk.id}
+              className="px-4 py-2 rounded-lg transition-all duration-200 cursor-pointer hover:bg-gray-300 hover:text-black"
+            >
+              {navlnk.title}
+            </Link>
+          );
+        })}
+      </div>
     </div>
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
