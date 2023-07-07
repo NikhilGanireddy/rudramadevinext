f8a84a0c356a605a205d18ad29c799044e025409
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-07-07 18:36:35 +0530

    sample 2

--- context/userContext.jsx
+++ context/userContext.jsx
@@ -1,30 +1,33 @@
-"use client"
-import {createContext, useContext, useEffect, useState} from "react";
+"use client";
+import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
-const GlobalUserContext = createContext({})
+const GlobalUserContext = createContext({});
 
-export const GlobalUserContextProvider = ({children}) => {
-    const router = useRouter()
-    const [userData, setUserData] = useState(null)
-    const [ready, setReady]= useState(false)
+export const GlobalUserContextProvider = ({ children }) => {
+  const router = useRouter();
+  const [userData, setUserData] = useState(null);
+  const [ready, setReady] = useState(false);
 
-    useEffect(() => {
-        if (!userData){
-            axios.get("/api/userdata", {withCredentials: true}).then(response => {
-                setUserData(response.data)
-                setReady(true)
-            }).then(() => router.refresh())
-        }
-    }, [])
+  useEffect(() => {
+    if (!userData) {
+      axios.get("/api/userdata", { withCredentials: true }).then((response) => {
+        setUserData(response.data);
+        setReady(true);
+      });
+    }
+  }, []);
 
-    // console.log(userData)
+  // console.log(userData)
 
-
-    return <GlobalUserContext.Provider value={{userData, setUserData,ready, setReady}}>
-        {children}
+  return (
+    <GlobalUserContext.Provider
+      value={{ userData, setUserData, ready, setReady }}
+    >
+      {children}
     </GlobalUserContext.Provider>
-}
+  );
+};
 
-export const useGlobalUserContext = () => useContext(GlobalUserContext)
+export const useGlobalUserContext = () => useContext(GlobalUserContext);
