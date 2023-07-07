c76f2b243a814887df3f7c8869fd76fcd110736f
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-07-07 18:25:58 +0530

    homepage login link

--- app/page.js
+++ app/page.js
@@ -5,15 +5,30 @@ import axios from "axios";
 import {useGlobalUserContext} from "@/context/userContext";
 
 export default function Home() {
-    const {userData} = useGlobalUserContext()
+    const {userData, setUserData, ready, setReady} = useGlobalUserContext()
+
+    if (!ready) return "Loading..."
+
+    if (ready && !userData) {
+        return <Link href={"/login"}/>
+    }
 
     return (
         <div className="p-4 rounded-xl border h-full flex flex-col justify-center items-center gap-6">
             <h1>Rudramadevi Girls Hostel</h1>
-            {userData ? <Link className="bg-black text-gray-300 px-6 py-2 rounded-lg border"
-                              href={`/${(userData?.isAdmin) ? "admin" : "user"}/dashboard`}>Go to Dashboard</Link>
-                : <Link className="bg-black text-gray-300 px-6 py-2 rounded-lg border" href={"/login"}>Login</Link>
-            }
+
+            <div>
+                {(ready && !userData) &&
+                    <Link className="bg-black text-gray-300 px-6 py-2 rounded-lg border" href={"/login"}>Login</Link>}
+
+                {(ready && userData) && <Link className="bg-black text-gray-300 px-6 py-2 rounded-lg border"
+                                              href={`/${(userData?.isAdmin) ? "admin" : "user"}/dashboard`}>Go to
+                    Dashboard</Link>
+                }
+            </div>
         </div>
     )
 }
+
+// <Link className="bg-black text-gray-300 px-6 py-2 rounded-lg border"
+//       href={`/${(userData?.isAdmin) ? "admin" : "user"}/dashboard`}>Go to Dashboard</Link>
