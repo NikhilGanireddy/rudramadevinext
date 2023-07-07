a03692c3acf50b43d26552ec7d1553ecc6b14482
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-07-07 19:49:26 +0530

    another token sample link 5

--- app/api/onrefreshgetdata/route.js
+++ app/api/onrefreshgetdata/route.js
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
-import {cookies} from "next/headers";
 import {NextResponse} from "next/server";
 import UserModel from "models/UserModel"
 import connectDB from "@/lib/mongoDatabase";
@@ -7,11 +6,15 @@ import connectDB from "@/lib/mongoDatabase";
 connectDB()
 
 export async function GET(request) {
-    const SECRET_KEY = "nikhilganireddy25345";
-    const tokendata = request.cookies.get("token")?.value
-    const decodeToken = jwt.verify(tokendata, SECRET_KEY)
-    // console.log(decodeToken.name)
-    const userdata = await UserModel.findById( decodeToken._id)
-    // console.log(userdata)
-    return NextResponse.json(userdata);
+    try {
+      const SECRET_KEY = "nikhilganireddy25345";
+      const tokendata = request.cookies.get("token")?.value
+      const decodeToken = jwt.verify(tokendata, SECRET_KEY)
+      // console.log(decodeToken.name)
+      const userdata = await UserModel.findById( decodeToken._id)
+      // console.log(userdata)
+      return NextResponse.json(userdata);
+    } catch (e) {
+      console.log(e)
+    }
 }
